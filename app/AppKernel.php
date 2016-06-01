bf9266de0c7dbd9eb9cd2835d5143c6e9ea43101
Norberto9507 <ngsoler@estudiantes.uci.cu>
2016-06-01 10:43:02 -0400

    Update AppKernel.php
    xcccc

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
-            new redBundle\redBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
             new pepeBundle\pepeBundle(),
         );
 
